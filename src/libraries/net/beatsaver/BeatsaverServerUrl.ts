7d5179f5c9b2bdebf9952ed8bb86a3ba0d0a60ab
kintawer <kintawer@gmail.com>
2022-05-09 01:15:30 +0400

    add url

--- src/libraries/net/beatsaver/BeatsaverServerUrl.ts
+++ src/libraries/net/beatsaver/BeatsaverServerUrl.ts
@@ -1,6 +1,7 @@
 enum BeatsaverServerUrl {
   Beatsaver = "https://beatsaver.com/api", // default, original
   BeatSaberPlus = "https://maps.beatsaberplus.com/api/", // mirror
+  NewApi = "https://api.beatsaver.com/",
 }
 
 export default BeatsaverServerUrl;
